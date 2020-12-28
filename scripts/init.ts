a5e4bfdf778c987cddb79463f7554b7898db9e94
Alex Thomas <cawfree@Alexs-MacBook-Pro.local>
2020-12-28 22:02:42 +0000

    chore: enable randomBytes

--- scripts/init.ts
+++ scripts/init.ts
@@ -44,6 +44,12 @@ const line = (message: string, padding: number = defaultPadding) =>
   fs.writeFileSync(
     index,
     `
+const {Platform} = require('react-native');
+
+if (Platform.OS !== 'web') {
+  require('react-native-get-random-values');
+}
+
 if (typeof Buffer === 'undefined') {
   global.Buffer = require('buffer').Buffer;
 }
@@ -68,6 +74,7 @@ registerRootComponent(App);
 
   const scripts = path.resolve(dir, 'scripts');
   const postinstall = path.resolve(scripts, 'postinstall.js');
+  const ganache = path.resolve(scripts, 'ganache.js');
   const pkg = path.resolve(dir, 'package.json');
 
   const yarn = path.resolve(dir, 'yarn.lock');
@@ -81,6 +88,15 @@ const {execSync} = require('child_process');
 execSync('npx pod-install', {stdio: 'inherit'});
     `.trim()
   );
+  fs.writeFileSync(
+    ganache,
+    `
+const {execSync} = require('child_process');
+execSync('node node_modules/.bin/ganache-cli', {stdio: 'inherit'});
+    `.trim()
+  );
+
+  // TODO: add jest, show a test with solc
 
   // package.json
   fs.writeFileSync(
@@ -89,7 +105,8 @@ execSync('npx pod-install', {stdio: 'inherit'});
       unflatten({
         ...flatten(JSON.parse(fs.readFileSync(pkg, 'utf-8'))),
         // scripts
-        'scripts.postinstall': 'node scripts/postinstall.js',
+        'scripts.postinstall': 'node scripts/postinstall',
+        'scripts.ganache': 'node scripts/ganache',
         // dependencies
         'dependencies.base-64': '1.0.0',
         'dependencies.buffer': '6.0.3',
@@ -98,6 +115,9 @@ execSync('npx pod-install', {stdio: 'inherit'});
         'dependencies.path-browserify': '0.0.0',
         'dependencies.react-native-stream': '0.1.9',
         'dependencies.react-native-crypto': '2.2.0',
+        'dependencies.react-native-get-random-values': '1.5.0',
+        // devDependencies
+        'devDependencies.ganache-cli': '6.12.1',
         // react-native
         'react-native.stream': 'react-native-stream',
         'react-native.crypto': 'react-native-crypto',
@@ -140,5 +160,41 @@ module.exports = {
 
     `.trim()
   );
-  // TODO: add node-libs-browser
+
+  // truffle
+  execSync(`cd ${dir}; npx truffle init;`, { stdio: 'inherit' });
+
+  // example
+  const app = path.resolve(dir, 'App.tsx');
+
+  fs.writeFileSync(
+    app,
+    `
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Web3 from 'web3';
+
+const styles = StyleSheet.create({
+  center: {alignItems: 'center', justifyContent: 'center'},
+});
+
+export default function App(): JSX.Element {
+  React.useEffect(() => {
+    (async () => {
+      const ganache = 'http://127.0.0.1:8545';
+      const web3 = new Web3(new Web3.providers.HttpProvider(ganache));
+      const latestBlock = await web3.eth.getBlock('latest');
+      const wallet = await web3.eth.accounts.create();
+      console.warn({ latestBlock });
+      console.warn({ wallet });
+    })();
+  }, []);
+  return (
+    <View style={[StyleSheet.absoluteFill, styles.center]}>
+      <Text>Welcome to Web3!</Text>
+    </View>
+  );
+}
+    `.trim()
+  );
 })();
